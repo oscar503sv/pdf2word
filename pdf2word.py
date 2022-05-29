48d2b811ae3f683e597b80eff738e43217853bba
Oscar Hernandez <ozkar@outlook.com>
2022-05-29 00:24:57 -0600

    add the ignore file

--- pdf2word.py
+++ pdf2word.py
@@ -8,14 +8,14 @@ word.visible = 0
 pdfs_path = "input_path" # path where .pdf files are stored
 for i, doc in enumerate(glob.iglob(pdfs_path+"*.pdf")):
     print("***************************************************************")
-    print("document found in: "+doc)
+    print("documento encontrado: "+doc)
     filename = doc.split('\\')[-1]
     in_file = os.path.abspath(doc)
-    print("converting file to .docx...")
+    print("convirtiendo documento a .docx...")
     wb = word.Documents.Open(in_file)
     out_file = os.path.abspath("output_path"+filename[0:-4]+ ".docx".format(i))
-    print("Output path:\n",out_file)
+    print("Documento de salida:\n",out_file)
     wb.SaveAs2(out_file, FileFormat=16) 
-    print("Success.")
+    print("Exitoso.")
     wb.Close()
 word.Quit()
